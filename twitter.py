d77893c8bd5e577e56f951213e78a60b7d0f4c8e
chessesio <flavianchessesio@gmail.com>
2017-10-07 14:35:17 +0000

    added class Twitter with basic functionality of the twitter clone | written initial main logic

--- twitter.py
+++ twitter.py
@@ -2,18 +2,32 @@ import user
 import tweet
 import comments
 
-logged_in = None
 
-class Twitter(user.User):
+class Twitter(user.User,):
 
     def __init__(self):
+        self.logged_in = None
         self.user_list = []
 
     def add_user(self,user):
         self.user_list.append(user)
 
     def sign_up(self):
+        self.add_user(new_user)
+
+    def sign_in(self,user):
+        self.logged_in = user
+
+    def sign_out(self):
+        self.logged_in = None
         
+def main():
+
+    print("Welcome The Twitter Clone")
+
+    option = int(input("1 New to twitter? Sign Up\n2 Already have an account? Log In\n"))
+
+    if option == 1:
         print("Welcome to twitter!\nJust a few details and we\'re done :)\n")
         
         name_ = input("Enter your name: ")
@@ -23,28 +37,29 @@ class Twitter(user.User):
         
         new_user = super().__init__(name=name_,username=username_,email=email_,password=password_)
 
-        self.add_user(new_user)
-
-    def sign_in(self):
-
+    if choice == 2:
         print("Welcome Back")
 
         name_ = input("Enter your username or email")
 
         for user in self.user_list:
             if user.username == name_ or user.email == name_:
-                logged_in_user = user
+                self.logged_in = user
                 print("You are now logged in")
                 return user
+
+                home = input("1 Tweet\n2 Retweet\n3 View Profile\n4 Log out\n\n")
+
+                for user in self.user_list:
+                    user.get_tweets
+
+
+
+
+
 
             else:
                 print("Wrong username or email!\nTry Again.")
 
-    def sign_out(self):
-        logged_in_user = None
-
-    def text_tweet()
-
-i = Twitter()
-i.sign_up()
-print(len(i.user_list))
+if __name__ == "__main__":
+    main()
