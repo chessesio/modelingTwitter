a1ceb3339aa6e11734050dfb5f390eb2c79a5f71
chessesio <flavianchessesio@gmail.com>
2017-10-07 13:56:46 +0000

    Deleted excess classes, all functionality in one class

--- tweet.py
+++ tweet.py
@@ -2,49 +2,12 @@ from datetime import datetime
 
 class Tweet:
 
-    def __init__(self,message,date_time,username,comments):
+    def __init__(self,username,message,picture,date_time,comments):
 
         self.username = username
         self.message = message
+        self.picture = None
         self.date_time = datetime.now()
-        self.comments = []
-
-    def add_comment(self,comment):
-        self.comments.append(comment)
-
-
-class TweetPic:
-
-    def _init__(self,picture,caption,date_time,username,comments):
-        self.username = username
-        self.picture = picture
-        self.caption = caption
-        self.date_time = datetime.now()
-        self.comments = []
-
-    def add_comment(self,comment):
-        self.comments.append(comment)
-
-
-class RetweetTweet:
-
-    def __init__(self,new_message,message,username,retweet_time,comments):
-
-        self.username = username
-        self.new_message = new_message
-        self.retweet_time = retweet_time
-        self.comments = []
-
-    def add_comment(self,comment):
-        self.comments.append(comment)
-
-class RetweetPic:
-
-    def __init__(self,new_message,picture,caption,username,retweet_time,comments):
-
-        self.username = username
-        self.new_message = new_message
-        self.retweet_time = retweet_time
         self.comments = []
 
     def add_comment(self,comment):
