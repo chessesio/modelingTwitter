742395ea4fd9f9a5793b361181df4b9f81478463
chessesio <flavianchessesio@gmail.com>
2017-10-07 22:18:15 +0000

    minor changes

--- twitter.py
+++ twitter.py
@@ -12,7 +12,8 @@ class Twitter(user.User,):
     def add_user(self,user):
         self.user_list.append(user)
 
-    def sign_up(self):
+    def sign_up(self, name_, username_, email_, password_):
+        new_user = super().__init__(name=name_,username=username_,email=email_,password=password_)
         self.add_user(new_user)
 
     def sign_in(self,user):
@@ -23,43 +24,56 @@ class Twitter(user.User,):
         
 def main():
 
-    print("Welcome The Twitter Clone")
 
-    option = int(input("1 New to twitter? Sign Up\n2 Already have an account? Log In\n"))
+    main_loop = 3
 
-    if option == 1:
-        print("Welcome to twitter!\nJust a few details and we\'re done :)\n")
-        
-        name_ = input("Enter your name: ")
-        username_ = input("Enter a username: ")
-        email_ = input("Enter an email address: ")
-        password_ = input("Enter a password: ")
-        
-        new_user = super().__init__(name=name_,username=username_,email=email_,password=password_)
+    while main_loop != 5:
 
-    if choice == 2:
-        print("Welcome Back")
+        print("Welcome The Twitter Clone")
 
-        name_ = input("Enter your username or email")
+        twit = Twitter()
+        option = int(input("1 New to twitter? Sign Up\n2 Already have an account? Log In\n"))
 
-        for user in self.user_list:
-            if user.username == name_ or user.email == name_:
-                self.logged_in = user
-                print("You are now logged in")
-                return user
+        if option == 1:
+            print("Welcome to twitter!\nJust a few details and we\'re done :)\n")
+            
+            name_ = input("Enter your name: ")
+            username_ = input("Enter a username: ")
+            email_ = input("Enter an email address: ")
+            password_ = input("Enter a password: ")
 
-                home = input("1 Tweet\n2 Retweet\n3 View Profile\n4 Log out\n\n")
+            twit.sign_up(name_, username_, email_, password_)
+            
+            
 
-                for user in self.user_list:
-                    user.get_tweets
+        if option == 2:
+            print("Welcome Back")
 
+            name_ = input("Enter your username or email: ")
 
-
-
-
-
+            for user in twit.user_list:
+                if user.username == name_ or user.email == name_ and :
+                    twit.logged_in = user
+                    print("You are now logged in")
+                    return user
+                    home()
             else:
                 print("Wrong username or email!\nTry Again.")
+
+def home():
+
+    twit = Twitter()
+    home = input("1 Tweet\n2 Retweet\n3 View Profile\n4 Log out\n\n")
+
+    for user in twit.user_list:
+        user.get_tweets
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
