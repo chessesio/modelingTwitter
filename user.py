1330e37e8683ff7d71321a586e0a0ef726fa71bc
chessesio <flavianchessesio@gmail.com>
2017-10-13 19:41:00 +0000

    Removed inherited tweets

--- user.py
+++ user.py
@@ -1,3 +1,7 @@
+# construct user object
+# user has a [name, username, email, password]<-- must have, profile picture and tweers[]
+# user can tweet or retweet
+
 class User:
 
     def __init__(self,name,username,email,password):
@@ -6,15 +10,18 @@ class User:
         self.username = username
         self.email = email
         self.password = password
-        self.prof_pic = ""
+        self.prof_pic = None
         self.tweets = []
 
-    def tweet_action(self,a_tweet):
-        self.tweets.append(a_tweet)
+#   user tweeting action
+    def tweet_action(self,username_,message_):
+        self.tweets.append(new_tweet)
 
+#   user can retweet
     def retweet_action(self,a_retweet):
         self.tweets.append(a_retweet)
 
+#   display a users tweets
     def get_tweets(self):
         for tweet in user.tweets:
             print(tweet)
